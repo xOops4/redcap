53b64cff24eaf56dc038e1a7ec26ad7762c46012
xOops4 <julien.burey@gmail.com>
2025-12-06 19:02:37 +0100

    update files tree

--- html/database.php
+++ html/database.php
@@ -16,7 +16,7 @@ $log_all_errors = FALSE;
 $hostname   = 'db';     //your_mysql_host_name
 $db         = 'redcap';     //your_mysql_db_name
 $username   = 'redcap';         //your_mysql_db_username
-$password   = 'Kenshin44';      //your_mysql_db_password
+$password   = 'Kenshin44++';      //your_mysql_db_password
 
 // You may optionally utilize a database connection over SSL/TLS for improved security. To do so, at minimum
 // you must provide the path of the key file, the certificate file, and certificate authority file.
